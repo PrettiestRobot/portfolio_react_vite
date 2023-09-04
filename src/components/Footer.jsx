f008d6826b1943e4246505700abbfb28bbc990ab
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-09-04 14:05:25 -0400

    loose ends

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import "../styles/footer.css";
+import "../styles/Footer.css";
 import githubLogo from "../assets/github-mark.svg";
 import linkedinLogo from "../assets/linkedin.svg";
 import facebookLogo from "../assets/facebook.svg";
