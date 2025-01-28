c496b0b0732bfea416c16f04161def5ca4963ee7
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2025-01-27 20:53:39 -0500

    media message update

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -9,7 +9,7 @@ function Media({ data }) {
       <div className="video-container">
         <div className="media-message">
           My reel is under construction. In the meantime, please enjoy this
-          visually interesting work sample
+          curated work sample.
         </div>
         <iframe
           width="600"
